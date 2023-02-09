7ec9b88c5ff785e094c971b5deceed8dbe4cfffa
Victor Garcés <victorgarcesgg@gmail.com>
2023-02-08 20:44:00 -0400

    add quit button

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Quit");
+            Application.Quit();
+        }
+    }
+}
